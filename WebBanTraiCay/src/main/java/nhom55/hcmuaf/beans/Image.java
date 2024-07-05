3ec98b69d1a91facd81a92e8118c64feeae34212
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-07-05 19:08:29 +0700

    UPDATE: add new product

--- WebBanTraiCay/src/main/java/nhom55/hcmuaf/beans/Image.java
+++ WebBanTraiCay/src/main/java/nhom55/hcmuaf/beans/Image.java
@@ -6,31 +6,22 @@ public class Image implements Serializable {
 
   private String id;
   private int productId;
-  private String url;
   private String imgPublicId;
   private String imgAssetId;
 
-  public Image(String id, String url, int productId) {
+  public Image(String id, int productId) {
     this.id = id;
-    this.url = url;
     this.productId = productId;
   }
 
-  public Image(int productId, String url, String imgPublicId, String imgAssetId) {
+  public Image(int productId, String imgPublicId, String imgAssetId) {
     this.productId = productId;
-    this.url = url;
     this.imgPublicId = imgPublicId;
     this.imgAssetId = imgAssetId;
   }
 
-  public Image(String id, String url) {
-    this.id = id;
-    this.url = url;
-  }
-
-  public Image(int productId, String url) {
+  public Image(int productId) {
     this.productId = productId;
-    this.url = url;
   }
 
 
@@ -43,14 +34,6 @@ public class Image implements Serializable {
 
   public void setId(String id) {
     this.id = id;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
   }
 
   public int getProductId() {
