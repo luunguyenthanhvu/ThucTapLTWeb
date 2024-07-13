2143e6ec3446c74072eed3eada4d758e8f13df4f
Vũ Hoàng Minh Tú <21130598@st.hcmuaf.edu.vn>
2024-07-13 20:30:20 +0700

    Them thanh toan bang Paypal

---
+++ WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/paypal/AuthorizePaymentServlet.java
@@ -0,0 +1,95 @@
+package nhom55.hcmuaf.controller.paypal;
+
+import com.paypal.base.rest.PayPalRESTException;
+import nhom55.hcmuaf.beans.Bills;
+import nhom55.hcmuaf.beans.Users;
+import nhom55.hcmuaf.beans.cart.Cart;
+import nhom55.hcmuaf.beans.cart.CartProduct;
+import nhom55.hcmuaf.util.MyUtils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "AuthorizePayPal", value = "/paypal/authorize-payment")
+public class AuthorizePaymentServlet  extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public AuthorizePaymentServlet() {
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Users users = MyUtils.getLoginedUser(session);
+        List<String> selectedProductIds =(List<String>) session.getAttribute("selectedProductIds");
+        Cart cart = (Cart) session.getAttribute("cart");
+        List<CartProduct> selectedProducts = new ArrayList<>();
+        if (cart != null && selectedProductIds != null) {
+            // get product list selected from cart
+            selectedProducts = cart.getSelectedProducts(selectedProductIds);
+        }
+        String productName = "";
+        //        Tổng tiền trái cây mà User định thanh toán
+        double subtotal = 0;
+        for(CartProduct cp : selectedProducts){
+            subtotal+=cp.getProducts().getPrice();
+            productName += cp.getProducts().getNameOfProduct() + ",";
+        }
+        String lastName = request.getParameter("ho_nguoi-dung");
+        String firstName = request.getParameter("ten_nguoi-dung");
+        String address = request.getParameter("dia-chi_nguoi-dung");
+        String city = request.getParameter("thanh-pho");
+        String district = request.getParameter("quan-huyen");
+        String phoneNumber = request.getParameter("sdt_nguoi-dung");
+        String email = request.getParameter("email_nguoi-dung");
+        String deliveryFee = request.getParameter("delivery_fee");
+        String cleanedString = deliveryFee.replaceAll("[₫\\s]", "");
+        cleanedString = cleanedString.replace(".", "");
+        double deliveryFeeDouble = Double.parseDouble(cleanedString);
+
+//       Mặc định cho thanh toán Paypal có idPayment = 2
+        int idPayment=2;
+        address+=address+", quận "+district+", tỉnh "+city;
+
+
+        Bills bills = new Bills();
+        bills.setProductList(productName);
+        bills.setStatus("Đang giao");
+        bills.setUserId(users.getId());
+        bills.setPayment(idPayment);
+        bills.setFirstName(firstName);
+        bills.setLastName(lastName);
+        bills.setStreetAddress(address);
+        bills.setCity(city);
+        bills.setPhoneNumber(phoneNumber);
+        bills.setEmail(email);
+        bills.setTotalPrice(subtotal);
+        bills.setDeliveryFee(deliveryFeeDouble);
+        try {
+            PaymentServices paymentServices = new PaymentServices();
+            String approvalLink = paymentServices.authorizePayment(bills);
+            session.setAttribute("lastName", lastName);
+            session.setAttribute("firstName", firstName);
+            session.setAttribute("address", address);
+            session.setAttribute("city", city);
+            session.setAttribute("phoneNumber", phoneNumber);
+            session.setAttribute("email", email);
+            session.setAttribute("subtotal", subtotal);
+            session.setAttribute("deliveryFee", deliveryFeeDouble);
+            response.sendRedirect(approvalLink);
+
+
+        } catch (PayPalRESTException ex) {
+            request.setAttribute("errorMessage", ex.getMessage());
+            ex.printStackTrace();
+//            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
+    }
+}
---
+++ WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/paypal/PaymentServices.java
@@ -0,0 +1,104 @@
+package nhom55.hcmuaf.controller.paypal;
+import java.util.*;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.*;
+import nhom55.hcmuaf.beans.Bills;
+
+public class PaymentServices {
+    private static final String CLIENT_ID = "AXjNIAvmyN7nqjWNdYrdjoX94D4vcaJ2LWHdXqz8qular76_jCvfH-uOSdCojCK-fgpo920v7bFA3Hm1";
+    private static final String CLIENT_SECRET = "EIVotgOAxlAu80qDQnGxTWjKb_MsI0hxy-LsqlAzPWoQepxn_DmODeEDxVjYDIhyP_zmqOBye41xrXS-";
+    private static final String MODE = "sandbox";
+
+    public String authorizePayment(Bills bill )
+            throws PayPalRESTException {
+
+        Payer payer = getPayerInformation();
+        RedirectUrls redirectUrls = getRedirectURLs();
+        List<Transaction> listTransaction = getTransactionInformation(bill);
+        Payment requestPayment = new Payment();
+        requestPayment.setTransactions(listTransaction);
+        requestPayment.setRedirectUrls(redirectUrls);
+        requestPayment.setPayer(payer);
+        requestPayment.setIntent("authorize");
+
+        APIContext apiContext = new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
+
+        Payment approvedPayment = requestPayment.create(apiContext);
+
+        return getApprovalLink(approvedPayment);
+
+    }
+
+    private Payer getPayerInformation() {
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        PayerInfo payerInfo = new PayerInfo();
+        payerInfo.setFirstName("William")
+                .setLastName("Peterson")
+                .setEmail("sb-crpg031604936@personal.example.com");
+
+
+        payer.setPayerInfo(payerInfo);
+
+        return payer;
+    }
+    private String getApprovalLink(Payment approvedPayment) {
+        List<Links> links = approvedPayment.getLinks();
+        String approvalLink = null;
+
+        for (Links link : links) {
+            if (link.getRel().equalsIgnoreCase("approval_url")) {
+                approvalLink = link.getHref();
+                break;
+            }
+        }
+        System.out.println(approvalLink);
+        return approvalLink;
+    }
+    private List<Transaction> getTransactionInformation(Bills bill) {
+        Details details = new Details();
+        details.setShipping(String.format("%.3f", 25000.0 / 25000.0));  // Trả về 1.00
+        details.setSubtotal(String.format("%.3f", 100000.0 / 25000.0));  // Trả về 4.00
+
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(String.format("%.3f", (100000.0 / 25000.0)+(25000.0 / 25000.0)));
+        amount.setDetails(details);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setDescription(bill.getProductList());
+
+        ItemList itemList = new ItemList();
+        List<Item> items = new ArrayList<>();
+
+        Item item = new Item();
+        item.setCurrency("USD");
+        item.setName(bill.getProductList());
+        item.setPrice(String.format("%.3f", 1000000.0 / 250000.0));  // Trả về 4.00
+        item.setQuantity("1");
+
+        items.add(item);
+        itemList.setItems(items);
+        transaction.setItemList(itemList);
+
+        List<Transaction> listTransaction = new ArrayList<>();
+        listTransaction.add(transaction);
+
+        return listTransaction;
+
+    }
+    private RedirectUrls getRedirectURLs() {
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:8080/page/shop/shop-forward");
+        redirectUrls.setReturnUrl("http://localhost:8080/paypal/summary-bill-paypal");
+
+        return redirectUrls;
+    }
+    public Payment getPaymentDetails(String paymentId) throws PayPalRESTException {
+        APIContext apiContext = new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
+        return Payment.get(apiContext, paymentId);
+    }
+}
